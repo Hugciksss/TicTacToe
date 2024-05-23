fca0629f69c63955c8c6566c93bc9321d2f3ff0d
Hugciksss <145644347+Hugciksss@users.noreply.github.com>
2024-05-23 10:00:30 +0300

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -5,25 +5,62 @@ mansLogs.title('TicTacToe')
 
 speletajsX=True
 count=0
+
+
+def checkWinner():
+    global winner
+    if(btn1["text"]=="X"and btn2["text"]=="X"and btn3["text"]=="X" or
+        btn4["text"]=="X"and btn5["text"]=="X"and btn6["text"]=="X" or
+        btn7["text"]=="X"and btn8["text"]=="X"and btn9["text"]=="X" or
+        btn1["text"]=="X"and btn4["text"]=="X"and btn7["text"]=="X" or
+        btn2["text"]=="X"and btn5["text"]=="X"and btn8["text"]=="X" or
+        btn3["text"]=="X"and btn6["text"]=="X"and btn9["text"]=="X" or
+        btn1["text"]=="X"and btn5["text"]=="X"and btn9["text"]=="X" or
+        btn3["text"]=="X"and btn5["text"]=="X"and btn7["text"]=="X"):
+        winner=True
+        messagebox.showinfo("TicTacToe", "Spēlētājs x ir uzvarētājs")
+    elif (btn1["text"]=="O"and btn2["text"]=="O"and btn3["text"]=="O" or
+        btn4["text"]=="O"and btn5["text"]=="O"and btn6["text"]=="O" or
+        btn7["text"]=="O"and btn8["text"]=="O"and btn9["text"]=="O" or
+        btn1["text"]=="O"and btn4["text"]=="O"and btn7["text"]=="O" or
+        btn2["text"]=="O"and btn5["text"]=="O"and btn8["text"]=="O" or
+        btn3["text"]=="O"and btn6["text"]=="O"and btn9["text"]=="O" or
+        btn1["text"]=="O"and btn5["text"]=="O"and btn9["text"]=="O" or
+        btn3["text"]=="O"and btn5["text"]=="O"and btn7["text"]=="O"):
+        winner=True
+        messagebox.showinfo("TicTacToe", "Spēlētājs O ir uzvarētājs")
+    elif count==9:
+        winner=False
+        messagebox.showinfo("TicTacToc", "Neizšķirts")
+
 def btnClick(button):#padod visu pogu
+
     global speletajsX,count#kādi main\igie tiks izmantoti visā programmā
-    if button["text"]==""and speletajsX==True:#spēlē spēlētājs x
+    if button["text"]==" "and speletajsX==True:#spēlē spēlētājs x
         button["text"]="X"#maina uz x
         speletajsX=False
         count+=1
+    elif button["text"]==" "and speletajsX==False:
+        button["text"]="O"
+        speletajsX=True
+        count+=1
+
+    else:
+        messagebox.showerror("TicTacToe","Šeit ir kāds ieklikšķinājis!")
+    checkWinner()
 
 
 
 
-btn1=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
-btn2=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
-btn3=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
-btn4=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
-btn5=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
-btn6=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
-btn7=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
-btn8=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
-btn9=Button(mansLogs,text='',width=6,height=3,bd=10,font=('Black'),bg= 'red')
+btn1=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn1))
+btn2=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn2))
+btn3=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn3))
+btn4=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn4))
+btn5=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn5))
+btn6=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn6))
+btn7=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn7))
+btn8=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn8))
+btn9=Button(mansLogs,text=' ',width=6,height=3,bd=10,font=('Black'),bg= 'red', command=lambda:btnClick(btn9))
 
 btn1.grid(row=1,column=0)
 btn2.grid(row=1,column=1)
